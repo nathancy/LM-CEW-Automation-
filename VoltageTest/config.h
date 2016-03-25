cfbd7ff5e24527f1f0b84c759440f5a329c5673b
Nathan <nathancy@hawaii.edu>
2016-03-24 17:26:53 -1000

    Adding config.h

---
+++ VoltageTest/config.h
@@ -0,0 +1,17 @@
+/***************************************
+ *
+ *
+ * 	File: config.h
+ * 	
+ * 	Configuration file to initialize the appropriate pins, 
+ * 	functions, and variables.
+ *
+ ***************************************/
+
+/* Check for previous includes */
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define VOLTAGE_PIN A0
+
+#endif
