3b9d7000a93d10815edd65ee4821fa943b086f2e
Nathan Lam <nathancy@hawaii.edu>
2016-05-11 13:50:59 -1000

    Adding isolated voltage module

---
+++ VoltageTest/CapacitorVoltage.ino
@@ -0,0 +1,61 @@
+/****************************************
+ * 
+ * File: CapacitorVoltage.ino
+ *      
+ * Isolated module to test voltage trigger
+ *    
+ ****************************************/
+ 
+/* External/program libraries */
+#include <CapacitiveSensor.h>
+#include "config.h"
+
+/* Create instance of sensor object */
+CapacitiveSensor cs_51_53(51, 53);  
+int voltage_count = 0;
+
+/******************************
+ * 
+ * Name:        setup
+ * Returns:     Nothing
+ * Parameter:   Nothing
+ * Description: Runs once at the start of the program.
+ *              Used for initialization.
+ * 
+ ******************************/
+void setup()                    
+{
+   /* Initialization */
+   cs_51_53.set_CS_AutocaL_Millis(0xFFFFFFFF);    
+   Serial.begin(9600);
+   pinMode(VOLTAGE_PIN, OUTPUT);
+   digitalWrite(VOLTAGE_PIN, LOW);
+}
+
+/******************************
+ * 
+ * Name:        loop
+ * Returns:     Nothing
+ * Parameter:   Nothing
+ * Description: Voltage will be logical low (0V) then logical high (5V)
+ * 
+ ******************************/
+void loop()                    
+{
+    /* Initially low voltage */
+    voltage_count++;
+    Serial.println("LOW");
+    Serial.println(voltage_count);
+
+    /* Switch to high voltage */
+    if(voltage_count >= 100)
+    {
+      digitalWrite(VOLTAGE_PIN, HIGH);
+      //digitalWrite(VOLTAGE_PIN, LOW);
+      Serial.println("HIGH");
+      voltage_count = 0;
+    }
+    
+    Serial.print("\n");
+    delay(100);                         
+}
---
+++ VoltageTest/config.h
@@ -0,0 +1,20 @@
+/****************************************
+ * 
+ * File: config.h
+ * 
+ * Configuration file to initialize the appropriate pins, 
+ * functions, and variables.
+ * 	 
+ ****************************************/
+
+/* Check for previous includes */
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* Voltage Pin to toggle wavefrom generator */
+#define VOLTAGE_PIN A0
+
+/* Number of capillary cells/circles in channel */
+#define CELLS 13
+
+#endif
